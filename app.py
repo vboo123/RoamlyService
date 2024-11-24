63060dcf1a70e69a324b57f77f82df5c598a7945
Vibhu Garg <vibhugarg@Vibhus-MBP.attlocal.net>
2024-11-24 13:13:43 -0800

    fix post request

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from cassandra.cluster import Cluster
 from cassandra.query import SimpleStatement
+from geolib import geohash
 import uuid
 
 app = FastAPI()
@@ -9,18 +10,26 @@ app = FastAPI()
 cluster = Cluster(['127.0.0.1'])  # Localhost
 session = cluster.connect()
 
+
+
+# Set some sample Geohashing
+latitude = 34.13425221700465 
+longitude = -118.32189112901952
+# Encode the coordinates into a geohash
+geohash_code = geohash.encode(latitude, longitude, precision=12)
+
 # Use the keyspace
 session.set_keyspace('roamly_keyspace')
 
 @app.post("/add-property/")
 async def add_property(
-    geohash_code: str,
-    name: str,
-    city: str,
-    country: str,
-    information_Chinese: str,
-    information_Indian: str,
-    information_British: str
+    # geohash_code: str,
+    # name: str,
+    # city: str,
+    # country: str,
+    # information_Chinese: str,
+    # information_Indian: str,
+    # information_British: str
 ):
     try:
         insert_query = SimpleStatement("""
@@ -28,14 +37,12 @@ async def add_property(
             information_Chinese, information_Indian, information_British) 
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
         """)
-        session.execute(insert_query, (
-            geohash_code, uuid.uuid4(), name, city, country, 
-            information_Chinese, information_Indian, information_British
-        ))
+        session.execute(insert_query, (geohash_code, uuid.uuid4(), 'Hollywood Sign', 'Los Angeles', 'USA', 
+                               "sdsd", "sdsdsdsd", "sdsds"))
+        
         return {"message": "Property added successfully"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
 @app.get("/get-properties/")
 async def get_properties():
     try:
@@ -44,7 +51,7 @@ async def get_properties():
         properties = [
             {
                 "geohash_code": row.geohash_code,
-                "property_id": str(row.property_id),
+                "property_id": str(row.property_id),  # Convert UUID to string
                 "name": row.name,
                 "city": row.city,
                 "country": row.country,
@@ -57,3 +64,4 @@ async def get_properties():
         return {"properties": properties}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+

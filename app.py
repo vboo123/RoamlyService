3529f7aa32dfcbbc0c41544d1f437ab315bbbf3f
Vibhu Garg <vibhugarg@Vibhus-MBP.attlocal.net>
2025-06-08 01:29:33 -0700

    us east 2

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ dynamodb = boto3.resource(
     'dynamodb',
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    region_name="us-east-1"
+    region_name="us-east-2"
 )
 landmarks_table = dynamodb.Table("Landmarks")
 users_table = dynamodb.Table("Users")
